81a3bdc6cafe208ad2b30acd7898736e19aaa574
Jordan Massiah <jmas@live.co.uk>
2018-02-25 20:09:10 +0000

    Fixed permissions for camera and storage read

--- app/src/main/java/com/example/jordan/steelrods/MainActivity.java
+++ app/src/main/java/com/example/jordan/steelrods/MainActivity.java
@@ -5,11 +5,14 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +22,7 @@ import android.widget.Button;
 import android.content.Intent;
 import android.provider.MediaStore;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -40,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     private Context mContext;
     Uri photoURI;
     File photoFile;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 10;
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 11;
 
     private Bitmap selectedBitmap = null;
 
@@ -73,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
         mGalleryButton = (Button) findViewById(R.id.home_button_2);
         mGalleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,8 +89,48 @@ public class MainActivity extends AppCompatActivity {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, REQUEST_LOAD_IMG);
+
+
             }
         });
+
+        // Checking for read storage permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an explanation
+            } else {
+                // No explanation need, request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        // Checking for camera permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            Toast.makeText(getApplicationContext(),"Permission not granted",Toast.LENGTH_SHORT).show();
+            // Show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation
+            } else {
+                // No explanation need, request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        } else {
+            // Permission has already been granted
+        }
+
     }
 
     @Override
@@ -144,4 +193,45 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    mGalleryButton.setEnabled(false);
+                }
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    mCameraButton .setEnabled(false);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
 }
